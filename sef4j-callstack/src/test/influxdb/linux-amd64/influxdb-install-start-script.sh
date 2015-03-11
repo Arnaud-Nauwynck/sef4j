8d12c78d117632b3d23342fa4985d3e574911c3c
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2015-03-11 21:27:45 +0100

    added InfluxDB script tests

---
+++ sef4j-callstack/src/test/influxdb/linux-amd64/influxdb-install-start-script.sh
@@ -0,0 +1,41 @@
+#
+# basic InfluxDB install+start script
+# see also file "influxdb-test.txt" for manual testing
+#
+#
+
+if [ ! -e build ]
+then
+	if [ ! -e influxdb-latest.amd64.tar.gz ]
+	then
+		wget https://s3.amazonaws.com/influxdb/influxdb-latest.amd64.tar.gz
+	else
+		echo "skip already done ... wget https://s3.amazonaws.com/influxdb/influxdb-latest.amd64.tar.gz"
+	fi
+
+	tar zxf influxdb-latest.amd64.tar.gz
+# unzipped content dir "build/":
+# $ find build
+# build
+# build/config.toml
+# build/influxdb-benchmark
+# build/scripts
+# build/scripts/influxdb-daemon.sh
+# build/scripts/post_install.sh
+# build/scripts/init.sh
+# build/influxdb
+# build/benchmark_config.toml
+
+
+mkdir -p influxdb-data/raft
+mkdir -p influxdb-data/wal
+
+else
+	echo "skip already done ... wget https://s3.amazonaws.com/influxdb/influxdb-latest.amd64.tar.gz ; tar zxf influxdb-latest.amd64.tar.gz"
+fi
+
+
+# ./build/influxdb --help
+
+./build/influxdb -config=config.toml
+
